838c8113e1b93eec3d335a37c2e197731eac8c34
krollquink <krollquink@gmail.com>
2018-04-13 01:01:16 +0000

    renew prime algo

--- prime.py
+++ prime.py
@@ -1,26 +1,24 @@
 
 
 
-number= []
-sieve = []
+number= list(range(1,100000000+1))
+sieve = [True] * 100000000
 prime = []
 
-for i in range(1,1000000+1):
-    number.append(i)
-    sieve.append(True)
-
-for i in range(2,9):
-    res = 2
-    while res *i <1000000+1:
-        sieve[( i * res )-1 ]=False 
-        res += 1
-#print(number)
-#print(sieve)
+for i in range(1,150000):
+    if sieve[i] == True:
+        res = number[i]
+        for j in range(2,200000000):
+            if j * res > len(sieve)-1:
+                break
+            sieve[(j*res)-1]=False 
+    else:
+        continue
 
 
-for j in range(0,len(sieve)-1):
+for j in range(1,len(sieve)-1):
     if sieve[j] == True:
         prime.append(number[j])
 
 print("There are",len(prime)-1,"prime number")
-print(prime[1:10001])
+print(prime[10001-1])
